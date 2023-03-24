f2f29ed2b751b70981d3b6910a03221ecd644ed1
matheusp94 <matheus.p940@gmail.com>
2023-03-24 09:40:44 -0300

    Março 24.2

--- projeto.py
+++ projeto.py
@@ -1,88 +1,100 @@
 # Menu principal de navegação. Contem:
-#1- preechimento dos dados do cliente 
-#2- Novo atendimento 
-#3- acompanhar atendimento 
-#4- cancelamento de um atendimento
+# 1- preechimento dos dados do cliente
+# 2- Novo atendimento
+# 3- acompanhar atendimento
+# 4- cancelamento de um atendimento
 
-def menu_navegacao():
+# A quiz game with a menu to register name, play the game, or end the program
 
-
-
-
-
-    # Assistente virtual
-
-
-    def executar_assistente():
-
-        print("Bem vindo ao assistente da Porto Seguro! Por favor, responda as seguintes perguntas: ")
-
-        nome = input("Qual o seu nome? ")
-        print(f"Olá, {nome}! Meu nome é Cirilo, sou o assistente da Porto Seguro. Para o seu atendimento, preciso que me informe o seu cpf: ")
-
-        cpf = input("Digite o seu cpf: ")
-        print(f"Muito obrigado, {nome}, registramos o seu cpf {cpf}! Agora, preciso que preencha o campo com uma das opções que melhor descreve sua resposta para cada pergunta que virá a seguir. \n")
-
-        # talvez por um timesleep aqui
-
-        # First question
-        while True:
-            print("Pergunta 1) De que atendimento você precisa?")
-            print("a) Novo atendimento")
-            print("b) Acompanhar atendimento")
-            print("c) Cancelar serviço agendado")
-            # print("d) Outro")
-
-            answer1 = input("Digite sua resposta: ")
-            if answer1.lower() == "a" or answer1.lower() == "novo atendimento":
-                print(
-                    "Ok, vamos fazer algumas perguntas para dar sequencia ao seu novo atendimento.")
-
-            elif answer1.lower() == "b" or answer1.lower() == "acompanhar atendimento":
-                input("Digite o ocorrido: \n")
-
-                break
-
-            else:
-                print("Incorrect. Please try again.")
-
-        # Second question
-        while True:
-            print("Question 2: Which is the largest desert in the world?")
-            print("a) Sahara")
-            print("b) Gobi")
-            print("c) Antarctica")
-            answer2 = input("Your answer: ")
-            if answer2.lower() == "a" or answer2.lower() == "sahara":
-                print("Correct!")
-                break
-            else:
-                print("Incorrect. Please try again.")
-
-        # Third question
-        while True:
-            print("Question 3: What is the tallest mountain in the world?")
-            print("a) K2")
-            print("b) Mount Everest")
-            print("c) Kilimanjaro")
-            answer3 = input("Your answer: ")
-            if answer3.lower() == "b" or answer3.lower() == "mount everest":
-                print("Correct!")
-                break
-            else:
-                print("Incorrect. Please try again.")
-
-        # Ask if user wants to play again
-        play_again = input("Deseja realizar um novo atendimento? (sim/não) ")
-        if play_again.lower() == "sim" or "s":
-            executar_assistente()
+def menu():
+    while True:
+        print("Please select an option:")
+        print("a) Register your name")
+        print("b) Play the game")
+        print("c) End the program")
+        choice = input("Your choice: ")
+        if choice.lower() == "a":
+            register_data()
+        elif choice.lower() == "b":
+            run_assistant()
+        elif choice.lower() == "c":
+            print("Thank you for playing! Goodbye!")
+            break
         else:
-            print("Agradecemos o seu contato. Tenha um ótimo restante de dia!")
+            print("Invalid choice. Please try again.")
+
+def register_data():
+    global name
+    name = input("What's your name? ")
+    cpf = input("What's your cpf? ")
+    placa_veiculo = input("What's your vehicle number? ")
+    alteracao_veiculo = input("What's your vehicle alterations? ")
+    numero_contato_wpp = input("Insira seu numero de contato: ")
+    email = ("Insira seu email: ")
+    imagem_veiculo = ("Insira uma imagem do seu veículo, com possíveis alterações feitas: ")
+    
+    
 
 
-# Call the function to start the quiz game
-    executar_assistente()
+# A quiz game with repeated questions and the option to play again
+def run_assistant():
+    print("Welcome to our quiz game! Please answer the following questions:")
 
-menu_navegacao()
+    global name
+    if not name:
+        name = input("What's your name? ")
+    print(f"Hello, {name}! Let's get started.")
+
+    # First question
+    while True:
+        print("Question 1: What is the capital of Canada?")
+        print("a) Toronto")
+        print("b) Ottawa")
+        print("c) Vancouver")
+        answer1 = input("Your answer: ")
+        if answer1.lower() == "b" or answer1.lower() == "ottawa":
+            print("Correct!")
+            break
+        else:
+            print("Incorrect. Please try again.")
+
+    # Second question
+    while True:
+        print("Question 2: Which is the largest desert in the world?")
+        print("a) Sahara")
+        print("b) Gobi")
+        print("c) Antarctica")
+        answer2 = input("Your answer: ")
+        if answer2.lower() == "a" or answer2.lower() == "sahara":
+            print("Correct!")
+            break
+        else:
+            print("Incorrect. Please try again.")
+
+    # Third question
+    while True:
+        print("Question 3: What is the tallest mountain in the world?")
+        print("a) K2")
+        print("b) Mount Everest")
+        print("c) Kilimanjaro")
+        answer3 = input("Your answer: ")
+        if answer3.lower() == "b" or answer3.lower() == "mount everest":
+            print("Correct!")
+            break
+        else:
+            print("Incorrect. Please try again.")
+
+    # Ask if user wants to play again
+    play_again = input("Do you want to play again? (yes/no) ")
+    if play_again.lower() == "yes":
+        run_assistant()
+    else:
+        print("Thank you for playing! Goodbye!")
+
+# Initialize the name variable
+name = None
+
+# Call the menu function to start the program
+menu()
 
 # End of program
